1e163b6df405e712386c970064878aadb0166f67
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2021-10-08 16:01:55 +0200

    displaying products but not with the good method

---
+++ front/js/cart.js
@@ -0,0 +1,2 @@
+const cart = JSON.parse(localStorage.getItem("cart"));
+console.log(cart);
---
+++ front/js/script.js
@@ -0,0 +1,30 @@
+const url = "http://localhost:3000/api/products";
+
+// CREATING VARIABLES
+const ITEMS = document.querySelector("#items");
+
+// const appendAnchorTag = () => {
+//   const anchor = document.createElement("a");
+//   const article = document.createElement("article");
+//   anchor.href = "./product.html?id=42";
+//   article.appendChild(anchor);
+// };
+
+// CREATING VARIABLES
+
+fetch(url).then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+    data.forEach((result) => {
+      console.log(result.name);
+      const products = `<a href=="./product.html?id=${result.id}">
+        <article>
+        <img src="${result.imageUrl}" alt="${result.altTxt}">
+        <h3>${result.name}</h3>
+        <p>${result.description}</p>
+        </article>
+      </a>`;
+      ITEMS.insertAdjacentHTML("beforeend", products);
+    });
+  });
+});
