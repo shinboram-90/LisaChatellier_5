d516d03348c6742ff0ad6951792eacb4e16aab6b
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2021-10-29 14:31:52 +0200

    can delete items, need to update price and quantity

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,80 +1,81 @@
 // const url = "http://localhost:3000/api/products";
-let cart = JSON.parse(localStorage.getItem("cart"));
+cart = JSON.parse(localStorage.getItem("cart")) || [];
 // console.log(cart);
 
-cart.forEach((item) => {
-  const article = document.createElement("article");
-  document.querySelector("#cart__items").appendChild(article);
-  article.className = "cart__item";
-  article.setAttribute("data-id", item._id);
+if (cart.length !== 0) {
+  cart.forEach((item) => {
+    const article = document.createElement("article");
+    document.querySelector("#cart__items").appendChild(article);
+    article.className = "cart__item";
+    article.setAttribute("data-id", item._id);
 
-  const imgParent = document.createElement("div");
-  imgParent.className = "cart__item__img";
+    const imgParent = document.createElement("div");
+    imgParent.className = "cart__item__img";
 
-  const image = document.createElement("img");
-  image.setAttribute("src", item.imageUrl);
-  image.setAttribute("alt", item.altTxt);
-  imgParent.appendChild(image);
+    const image = document.createElement("img");
+    image.setAttribute("src", item.imageUrl);
+    image.setAttribute("alt", item.altTxt);
+    imgParent.appendChild(image);
 
-  const contentParent = document.createElement("div");
-  contentParent.className = "cart__item__content";
+    const contentParent = document.createElement("div");
+    contentParent.className = "cart__item__content";
 
-  const priceParent = document.createElement("div");
-  priceParent.className = "cart__item__content__titlePrice";
+    const priceParent = document.createElement("div");
+    priceParent.className = "cart__item__content__titlePrice";
 
-  const h2 = document.createElement("h2");
-  h2.innerText = item.name;
+    const h2 = document.createElement("h2");
+    h2.innerText = item.name;
 
-  const pPrice = document.createElement("p");
-  let priceSameItems = parseInt(item.price) * parseInt(item.quantity);
-  pPrice.innerText = `${priceSameItems} €`;
+    const pPrice = document.createElement("p");
+    let priceSameItems = parseInt(item.price) * parseInt(item.quantity);
+    pPrice.innerText = `${priceSameItems} €`;
 
-  const pClr = document.createElement("p");
-  pClr.innerText = item.color;
+    const pClr = document.createElement("p");
+    pClr.innerText = item.color;
 
-  const settingsParent = document.createElement("div");
-  settingsParent.className = "cart__item__content__settings";
+    const settingsParent = document.createElement("div");
+    settingsParent.className = "cart__item__content__settings";
 
-  const quantityParent = document.createElement("div");
-  quantityParent.className = "cart__item__content__settings__quantity";
+    const quantityParent = document.createElement("div");
+    quantityParent.className = "cart__item__content__settings__quantity";
 
-  const pQty = document.createElement("p");
-  pQty.innerText = `Qté : ${item.quantity}`;
-  // console.log(pQty);
+    const pQty = document.createElement("p");
+    pQty.innerText = `Qté : ${item.quantity}`;
+    // console.log(pQty);
 
-  const input = document.createElement("input");
-  input.className = "itemQuantity";
-  input.setAttribute("type", "number");
-  input.setAttribute("name", "itemQuantity");
-  input.setAttribute("min", "1");
-  input.setAttribute("max", "100");
-  input.setAttribute("value", item.quantity);
+    const input = document.createElement("input");
+    input.className = "itemQuantity";
+    input.setAttribute("type", "number");
+    input.setAttribute("name", "itemQuantity");
+    input.setAttribute("min", "1");
+    input.setAttribute("max", "100");
+    input.setAttribute("value", item.quantity);
 
-  const deleteParent = document.createElement("div");
-  deleteParent.className = "cart__item__content__settings__delete";
+    const deleteParent = document.createElement("div");
+    deleteParent.className = "cart__item__content__settings__delete";
 
-  const deleteItem = document.createElement("p");
-  deleteItem.className = "deleteItem";
-  deleteItem.innerText = "Supprimer";
-  deleteParent.appendChild(deleteItem);
+    const deleteItem = document.createElement("p");
+    deleteItem.className = "deleteItem";
+    deleteItem.innerText = "Supprimer";
+    deleteParent.appendChild(deleteItem);
 
-  deleteItem.addEventListener("click", (e) => {
-    const elt = e.target;
-    const parent = elt.parentNode;
-    const gdParent = parent.parentNode;
-    const gdGdParent = gdParent.parentNode;
-    const ancestor = gdGdParent.parentNode;
-    // console.log(ancestor);
-    areYouSure(item);
+    deleteItem.addEventListener("click", (e) => {
+      e.preventDefault();
+      const elt = e.target;
+      const ancestor = elt.closest("article");
+      areYouSure(item, ancestor);
+    });
+
+    //calling function when multiple children;
+    appendChildren(article, [imgParent, contentParent]);
+    appendChildren(contentParent, [priceParent, settingsParent]);
+    appendChildren(priceParent, [h2, pClr, pPrice]);
+    appendChildren(settingsParent, [quantityParent, deleteParent]);
+    appendChildren(quantityParent, [pQty, input]);
   });
-
-  //calling function when multiple children;
-  appendChildren(article, [imgParent, contentParent]);
-  appendChildren(contentParent, [priceParent, settingsParent]);
-  appendChildren(priceParent, [h2, pClr, pPrice]);
-  appendChildren(settingsParent, [quantityParent, deleteParent]);
-  appendChildren(quantityParent, [pQty, input]);
-});
+} else {
+  console.log("Your cart is empty");
+}
 
 function appendChildren(parent, children) {
   children.forEach((child) => {
@@ -98,20 +99,25 @@ function total() {
 
 total();
 // localStorage.clear();
-// const areYouSure = (item) => {
-//   if (
-//     window.confirm(
-//       `Vous etes sur le point de supprimer ${item.quantity} ${item.name} de couleur ${item.color} de votre panier.`
-//     )
-//   ) {
-//     alert("L'article a ete supprime");
-//   } else {
-//     alert("Aucune modification n'a ete apportee");
-//   }
-// };
 
-const areYouSure = (item) => {
-  console.log(cart);
-  console.log(`ceci est un console log avec concatenation ${cart}`);
-  console.log(item);
+const areYouSure = (item, ancestor) => {
+  if (
+    window.confirm(
+      `Vous êtes sur le point de supprimer ${item.quantity} ${item.name} de couleur ${item.color} de votre panier.`
+    )
+  ) {
+    cart.forEach((key, value) => {
+      if (key._id === item._id && key.color === item.color) {
+        value = cart.indexOf(item);
+        cart.splice(value, 1);
+      } else {
+        console.log("Not the right item");
+      }
+      ancestor.remove();
+      localStorage.setItem("cart", JSON.stringify(cart));
+    });
+    alert("Article(s) supprimé(s) avec succès");
+  } else {
+    alert("Aucune modification n'a été apportée");
+  }
 };
